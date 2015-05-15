2d04310d074bf9fab45ebe36a2b08144b308dd70
Vinicius Cruz <vinaocruz@gmail.com>
2015-05-15 19:09:11 -0300

    Commit inicial de sistema de ecommerce

---
+++ ecommerce/cadastro_cliente.php
@@ -0,0 +1,64 @@
+<html>
+
+	<body>
+
+		<h1>Cadastro de Cliente</h1>
+
+		<form method="POST" action="">
+			<p>
+				Nome:<br />
+				<input type="text" name="nome" />
+			</p>
+			<p>
+				Telefone:<br />
+				<input type="text" name="telefone" />
+			</p>
+			<p>
+				E-mail:<br />
+				<input type="text" name="email" />
+			</p>
+			<p>
+				CPF:<br />
+				<input type="text" name="cpf" />
+			</p>
+			<p>
+				Login:<br />
+				<input type="text" name="login" />
+			</p>
+			<p>
+				Senha:<br />
+				<input type="password" name="senha" />
+			</p>
+
+			<h2>Endereço</h2>
+			<p>
+				Rua:<br />
+				<input type="text" name="rua" />
+			</p>
+			<p>
+				Número:<br />
+				<input type="text" name="numero" />
+			</p>
+			<p>
+				Bairro:<br />
+				<input type="text" name="bairro" />
+			</p>
+			<p>
+				Cidade:<br />
+				<input type="text" name="cidade" />
+			</p>
+			<p>
+				Estado:<br />
+				<input type="text" name="estado" />
+			</p>
+			<p>
+				CEP:<br />
+				<input type="text" name="cep" />
+			</p>
+
+			<input type="submit" value="Enviar" />
+		</form>
+
+	</body>
+
+</html>
