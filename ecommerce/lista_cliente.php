d7c84b784f42c1e3e7b38e9b54a8f5eb1ca225d2
Vinicius Cruz <vinaocruz@gmail.com>
2015-05-15 19:31:51 -0300

    Html para listagem de clientes

---
+++ ecommerce/lista_cliente.php
@@ -0,0 +1,26 @@
+<html>
+
+	<body>
+		<h1>Lista de Clientes</h1>
+		<table>
+			<tr>
+				<td>Nome</td>
+				<td>E-mail</td>
+				<td>CPF</td>
+				<td></td>
+			</tr>
+			
+			<tr>
+				<td>Joaquim</td>
+				<td>joaquim@gmail.com</td>
+				<td>787.322.552-68</td>
+				<td>
+					<a href="excluir_cliente.php">[ Excluir ]</a>
+				</td>
+			</tr>
+
+		</table>
+
+	</body>
+
+</html>
